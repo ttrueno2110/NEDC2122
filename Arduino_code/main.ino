8c220df1d014175703556dfd437e0c4d4b88a082
Toyota Trueno <kerodgy1@gmail.com>
2021-10-26 14:44:27 -0400

    added ino file

---
+++ Arduino_code/main.ino
@@ -0,0 +1,12 @@
+/*
+Test code stuff - deploy this to github, import into repl/vscode
+to edit there, then send back here when working in class or deploying on chromebooks
+*/
+
+void setup() {
+    
+}
+
+void loop() {
+    
+}
