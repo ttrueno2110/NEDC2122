d7804a01ce1e9d5d6ec14e3b3184586bb82b039f
VijayMaheswaran <>
2021-12-07 19:32:39 +0000

    test

--- Arduino_code/main.ino
+++ Arduino_code/main.ino
@@ -1,10 +1,10 @@
 /*
 Test code stuff - deploy this to github, import into repl/vscode
-to edit there, then send back here when working in class or deploying on chromebooks
+to edit there, then send back here when working in class or deploying on chromebooks hello world! Test 
 */
 
 void setup() {
-    
+    pinMode ();
 }
 
 void loop() {
